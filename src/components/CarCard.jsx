acd95d9364a034782b4788c4ee4b582695e37cfe
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2023-11-13 11:01:14 -0300

    card de carros

---
+++ src/components/CarCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../styles/CarCard.css'
+
+function CarCard({ brandName, brandLogo, carImg, model, year, price }) {
+  return (
+    <div className='car-card'>
+        <img src={carImg} className='car-img' />
+        <div className="car-brand">
+            <img src={brandLogo} className='car-brand-logo' />
+            {brandName}
+        </div>
+        <div className="car-details">
+          <div className="car-info">
+            <h3>{model} - {year}</h3>
+            <p>R$ {price}</p>
+          </div>
+          <div className="car-button">
+            <input type="button" value="Ver mais" />
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default CarCard
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import '../styles/App.css'
+import CarCard from '../components/CarCard'
 
 function App() {
 
   return (
     <>
+      <CarCard carImg='
+https://pics.porsche.com/rtt/iris?COSY-EU-100-1713c6eK12UC31P3T5JOCU%25hjdmiTDDmvMXlHWguCuq6QpcEUSu%258BgymCzGoajsEwhaxFH8dFMR6%258sMex58EP8nESW323U8iJZ2GP1iPsFmlKmjXKQo4gto6ZohJwkQjfDrhJDVougpBoV60KZOvWTQZB1aifSs%251BDYsHqZ8CQfUSkBpIAYC890lPomKSv6XR8Y4WDOgP3akKUnw1CYTEeqpFiLRZmhbgmFpq6QfiIpggyxc7qr7RsWmwZ76HnaXm1eQzX2cQ8' brandName='Porsche' brandLogo='https://murrugadesign.files.wordpress.com/2013/03/logo-porsche.jpg?w=834' model='911 Carrera' year='2024' price='460.000,000'></CarCard>
     </>
   )
 }
