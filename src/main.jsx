4b9b1acf8449bc8b2bb00c0caefb82891b696ac5
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2023-11-13 11:40:49 -0300

    página not found e router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/App.jsx'
+import NotFound from './pages/NotFound.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "not-found",
+    element: <NotFound />,
+  },
+  // {
+  //   path: "profile",
+  //   element: <Profile />,
+  // },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <RouterProvider router={router} />
+);
+
+// reportWebVitals();
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div className='not-found'>
+      <h1>Erro 404</h1>
+      <p>Página não encontrada!</p>
+    </div>
+  )
+}
+
+export default NotFound
