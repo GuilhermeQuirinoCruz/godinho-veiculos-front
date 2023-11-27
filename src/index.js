8ab8b7477cffba11f440ee33f82ff8ac19cfcd77
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2023-11-27 11:12:02 -0300

    axios default base url

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import { BrowserRouter as Router } from "react-router-dom";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://us-central1-godinho-veiculos.cloudfunctions.net";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
