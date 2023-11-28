88710d7f921ab2a7e200dfc60969e16c9eab88e5
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2023-11-28 19:37:50 -0300

    cilindradas, objeto carro concluído, css textarea

--- src/pages/CadastroCarro.jsx
+++ src/pages/CadastroCarro.jsx
@@ -19,6 +19,7 @@ function CadastroCarro() {
   const exchangeRef = useRef('');
   const fuelRef = useRef('');
   const steeringRef = useRef('');
+  const powerRef = useRef(0);
   const kilometragemRef = useRef(0);
   const portasRef = useRef(0);
   const placaRef = useRef('');
@@ -88,13 +89,13 @@ function CadastroCarro() {
         name: modeloRef.current.value,
         photos: fotos,
         vehicle: {
-          brand: "brand",
+          brand: marcas.find(marca => marca.code === marcaRef.current.value).name,
           model: modeloRef.current.value,
           year: anoRef.current.value,
           exchange: exchangeRef.current.value,
           fuel: fuelRef.current.value,
           steering: steeringRef.current.value,
-          power: "1.4",
+          power: powerRef.current.value,
           type: "Carros, vans e utilitários",
           mileage: kilometragemRef.current.value,
           doors: portasRef.current.value,
@@ -115,6 +116,7 @@ function CadastroCarro() {
         <h6 className="fw-bold mb-4">Dados do Carro</h6>
         <Form onSubmit={cadastrarCarro}>
           <FormGroup className="contact__form">
+            <label htmlFor="sltMarca">Marca *</label>
             <select name="sltMarca" id="sltMarca" onChange={selecionarMarca} ref={marcaRef}>
               {marcas.map((marca) => {
                 return (
@@ -127,6 +129,7 @@ function CadastroCarro() {
           </FormGroup>
 
           <FormGroup className="contact__form">
+            <label htmlFor="sltModelo">Modelo *</label>
             <select name="sltModelo" id="sltModelo" ref={modeloRef}>
               {modelos.length === 0 ?
                 <option key="carregando" value="0">
@@ -143,36 +146,45 @@ function CadastroCarro() {
           </FormGroup>
 
           <FormGroup className="contact__form">
+            <label htmlFor="txtDescricao">Descrição *</label> <br />
             <textarea
               rows="2"
               placeholder="Descrição do carro"
               className="textarea"
+              id="txtDescricao"
+              name="txtDescricao"
               ref={descricaoRef}
             ></textarea>
           </FormGroup>
 
           <FormGroup className="contact__form">
+            <label htmlFor="txtFotos">Fotos *</label> <br />
             <textarea
               rows="4"
               placeholder="Fotos, separadas por linha"
               className="textarea"
+              id="txtFotos"
+              name="txtFotos"
               ref={fotosRef}
             ></textarea>
           </FormGroup>
 
           <FormGroup className="contact__form">
-            <Input placeholder="Ano" type="number" innerRef={anoRef} />
+            <label htmlFor="nmbAno">Ano *</label>
+            <Input name="nmbAno" id="nmbAno" placeholder="Ano" type="number" innerRef={anoRef} />
           </FormGroup>
 
           <FormGroup className="contact__form">
-            <select name="exchange" id="exchange" ref={exchangeRef}>
+          <label htmlFor="sltExchange">Câmbio *</label>
+            <select name="sltExchange" id="sltExchange" ref={exchangeRef}>
               <option key="exchange-automatico" value="Automático">Automático</option>
               <option key="exchange-manual" value="Manual">Manual</option>
             </select>
           </FormGroup>
 
           <FormGroup className="contact__form">
-            <select name="fuel" id="fuel" ref={fuelRef}>
+          <label htmlFor="sltFuel">Tipo de Combustível *</label>
+            <select name="sltFuel" id="sltFuel" ref={fuelRef}>
               <option key="fuel-gasolina" value="Gasolina">Gasolina</option>
               <option key="fuel-diesel" value="Diesel">Diesel</option>
               <option key="fuel-eletrico" value="Elétrico">Elétrico</option>
@@ -180,7 +192,8 @@ function CadastroCarro() {
           </FormGroup>
 
           <FormGroup className="contact__form">
-            <select name="steering" id="steering" ref={steeringRef}>
+            <label htmlFor="sltSteering">Tipo de Direção *</label>
+            <select name="sltSteering" id="sltSteering" ref={steeringRef}>
               <option key="steering-hidraulica" value="Hidráulica">Hidráulica</option>
               <option key="steering-eletrica" value="Elétrica">Elétrica</option>
               <option key="steering-manual" value="Manual">Manual</option>
@@ -188,19 +201,28 @@ function CadastroCarro() {
           </FormGroup>
 
           <FormGroup className="contact__form">
-            <Input placeholder="Kilometragem" type="number" innerRef={kilometragemRef} />
+            <label htmlFor="nbmPower">Cilindradas *</label>
+            <Input name="nbmPower" id="nbmPower" placeholder="1.0, 1,4, 1.6, ..." type="number" step="0.1" innerRef={powerRef} />
           </FormGroup>
 
           <FormGroup className="contact__form">
-            <Input placeholder="Portas" type="number" innerRef={portasRef} />
+            <label htmlFor="nmbKm">Kilometragem *</label>
+            <Input name="nmbKm" id="nmbKm" placeholder="Kilometragem" type="number" innerRef={kilometragemRef} />
           </FormGroup>
 
           <FormGroup className="contact__form">
-            <Input placeholder="Placa" type="text" innerRef={placaRef} />
+            <label htmlFor="nbmPortas">Portas *</label>
+            <Input name="nbmPortas" id="nbmPortas" placeholder="Portas" type="number" innerRef={portasRef} />
           </FormGroup>
 
           <FormGroup className="contact__form">
-            <Input placeholder="Cor" type="text" innerRef={corRef} />
+            <label htmlFor="txtPlaca">Placa *</label>
+            <Input name="txtPlaca" id="txtPlaca" placeholder="Placa" type="text" innerRef={placaRef} />
+          </FormGroup>
+
+          <FormGroup className="contact__form">
+            <label htmlFor="txtCor">Cor *</label>
+            <Input name="txtCor" id="txtCor" placeholder="Cor" type="text" innerRef={corRef} />
           </FormGroup>
 
           <button className=" contact__btn" type="submit">
