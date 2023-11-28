0f01e38bec0255a975813c906dde219a026e21a2
BryanRibeiroBento <bryanribeiro.bento@gmail.com>
2023-11-28 10:09:40 -0300

    fiz um tanto de coisa

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -2,13 +2,8 @@ import React from "react";
 import "../../styles/header.css";
 
 const Helmet = (props) => {
-<<<<<<< HEAD
   document.title = "Godinho Veículos - " + props.title;
   return <div className="w-100">{props.children}</div>;
-=======
-  document.title = "Rent Car Service - " + props.title;
-  return <div className="w-100 cor">{props.children}</div>;
->>>>>>> 308aaed (fiz um tanto de coisa)
 };
 
 export default Helmet;
